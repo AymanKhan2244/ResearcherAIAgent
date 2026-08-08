f2cb09424a28f304a83d8b375035a96f3336ec1a
Ayman khan <aymnakhan.aiml22@gmail.com>
2026-08-08 13:37:03 +0530

    feat: initialize environment variables for Groq, LangSmith, and Tavily API keys

--- model.py
+++ model.py
@@ -5,6 +5,10 @@ import os
 load_dotenv()
 
 
+os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
+os.environ["LANGSMITH_API_KEY"] = os.getenv("LANGSMITH_API_KEY")
+os.environ["TAVILY_API_KEY"] = os.getenv("TAVILY_API_KEY")
+
 llm = ChatGroq(
     model="llama-3.3-70b-versatile",
     max_tokens=1024
